2b5a6c0fb3aa1131346a7a9e0b8492a1a8ee139e
Christian Wolf <github@christianwolf.email>
2020-02-18 09:58:16 +0100

    Added automatic setup run using entrypoint script

--- docker/development/app/entrypoint.sh
+++ docker/development/app/entrypoint.sh
@@ -11,6 +11,25 @@ fi
 
 sudo -u www-data composer install
 
+if [ "x$PARTKEEPR_FORCE_UPDATE" = "xyes" ]; then
+	
+	# Clears the production cache
+	sudo -u www-data php app/console cache:clear --env=prod
+	
+	# Executes the database migrations
+	sudo -u www-data php app/console doctrine:migrations:migrate --no-interaction
+	
+	# Updates the database schema
+	sudo -u www-data php app/console doctrine:schema:update --force
+	
+	# Builds all required files and warms up the cache
+	sudo -u www-data ./vendor/bin/phing
+	
+	# Runs all crons
+	sudo -u www-data php app/console partkeepr:cron:run
+	
+fi
+
 if [ -n "$ADD_PHPINFO_FILE" ]; then
 	sudo -u www-data cp /var/www/html/phpinfo.php /var/www/pk/web
 fi
