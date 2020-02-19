87392d0a0f966939ef8bb47f765d5285b5030390
Christian Wolf <github@christianwolf.email>
2020-02-19 15:53:53 +0100

    Enabled building of all images

--- docker/production/check-build.sh
+++ docker/production/check-build.sh
@@ -12,8 +12,11 @@ if [ $ret -eq 0 ]; then
 
 	# A valid tag was found
 	
+	docker build -t $url/production:latest --build-arg SRC_IMAGE=$url/base:$(git describe) $(dirname "$0")
+	
 	docker tag $url/production:latest $url/production:v$tag
 	docker push $url/production:v$tag
+
 else
 
 	echo "Do not build as no tag was found"
